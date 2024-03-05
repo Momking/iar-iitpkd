f19c0c7a706e3d6dde97921257cd1e27756c91cc
Nishant <nishant.dqy@gmail.com>
2024-03-05 18:24:46 +0530

    react-router-dom installed

--- iar-iitpkd/src/main.jsx
+++ iar-iitpkd/src/main.jsx
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import {Routes, Route} from "react-router-dom";
 
 // Importing components
 import NavbarGlobal from "./components/global/NavbarGlobal";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>
-    <NavbarGlobal />
-  </div>
+  <Routes>
+    <Route path="/" element={<NavbarGlobal />} />
+  </Routes>
 );
